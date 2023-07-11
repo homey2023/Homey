b4178bb86e35764f8d2e98588518cddd5b31c180
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-07-11 15:34:15 +0900

    FEAT: Introduce anormaly detection in api

--- server/apis/home_safety_rating.py
+++ server/apis/home_safety_rating.py
@@ -9,15 +9,39 @@ and anomaly detection on that data.
 
 import os
 import sys
+import pickle
 from flask import request
 from flask import jsonify
 from flask_restx import Namespace, Resource
+from deepod.models.icl import ICL
+from deepod.models.dif import DeepIsolationForest
+from deepod.models.slad import SLAD
+
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from dataloader.dataloader import count_place  # Update the import statement
 import numpy as np
 
 home_safety_rating_api = Namespace('home_safety_rating')
 
+models = {
+    'icl': ICL(),
+    'dif': DeepIsolationForest(),
+    'slad': SLAD(),
+}
+
+def get_model(model_name):
+    try:
+        model = models[model_name]
+        model.load_state_dict(torch.load(f'./weight/{model_name}.pt'))
+    except KeyError:
+        model = models['icl']
+        model.load_state_dict(torch.load('./weight/icl.pt'))
+    return model
+
+def load_scaler(feature):
+    with open(f'./weight/scaler_{feature}.pkl', 'rb') as f:
+        return pickle.load(f)
+    
 @home_safety_rating_api.route('/count')
 class Count(Resource):
     """
@@ -92,22 +116,26 @@ class Anomaly(Resource):
         Query Parameters:
         latitude -- the latitude of the location
         longitude -- the longitude of the location
+        model -- the model to use for anomaly detection (optional)
 
         Returns:
         A JSON object mapping safety feature types to boolean values indicating 
         whether an anomaly was detected for each feature.
         """
-        # Get latitude and longitude from the query parameters
+        # Get latitude, longitude, and model from the query parameters
         latitude = float(request.args.get('latitude'))
         longitude = float(request.args.get('longitude'))
+        model_name = request.args.get('model')
         
         # Call the dataloader function
         result = count_place(latitude, longitude)
         
-        # Detect anomalies
-        counts = np.array(list(result.values()))
-        mean = counts.mean()
-        std = counts.std()
-        anomalies = (np.abs(counts - mean) > 2 * std).tolist()
+        # Load the scalers and scale the data
+        counts = np.array([load_scaler(feature).transform([[count]]) for feature, count in result.items()]).reshape(1, -1)
         
-        return {"anomalies": anomalies}
+        # Get the model and detect anomalies
+        model = get_model(model_name)
+        anomalies = model.decision_function(counts)
+        
+        return {"anomalies": anomalies.tolist()}
+    
