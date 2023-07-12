65c4d6b4cf06a1137fb56732a130940d10401cc6
MJLaboratory <mjluckk@gmail.com>
2023-07-12 23:54:16 +0900

    changed bus_stop.csv file

--- server/dataloader/dataloader.py
+++ server/dataloader/dataloader.py
@@ -48,7 +48,7 @@ def count_place(latitude, longitude, radius=1,
         "street_lamp_v1.csv", 
         "women_protective_house_v1.csv",
         "women_protective_parcel_v1.csv",
-        "bus_stop.csv"
+        "bus_stop_v1.csv"
     ]):
     """
     Count the number of geographical features from various .csv files 
